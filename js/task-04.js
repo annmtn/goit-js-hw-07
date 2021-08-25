6e44ef688cc095182419f4ae9090132e3dde0396
Anna Mitina <anndan5810@gmail.com>
2021-08-26 01:11:20 +0300

    Правки в задаче №4
    Добавила переменную счетчик

--- js/task-04.js
+++ js/task-04.js
@@ -4,19 +4,29 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
-const counterValueEl = document.querySelector('#value');
+const valueEl = document.querySelector("#value");
 const btnDecrementEl = document.querySelector('[data-action="decrement"]');
 const btnIncrementEl = document.querySelector('[data-action="increment"]');
 
+// btnDecrementEl.addEventListener("click", function decrement() {
+//   valueEl.textContent -= 1;
+// });
 
-btnDecrementEl.addEventListener('click', 
-    function decrement() {
-        counterValueEl.textContent -= 1
-    }
-)
+// btnIncrementEl.addEventListener("click", function increment() {
+//   valueEl.textContent = Number(valueEl.textContent) + 1;
+// });
 
-btnIncrementEl.addEventListener('click', 
-    function increment() {
-        counterValueEl.textContent = Number(counterValueEl.textContent) + 1
-    }
-)
+let counterValue = 0;
+
+btnDecrementEl.addEventListener("click", decrement);
+btnIncrementEl.addEventListener("click", increment);
+
+function decrement() {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+}
+
+function increment() {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+}
